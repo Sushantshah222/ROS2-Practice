2163779fd01a00b2a1235e3fe31c511fe1609e2f
sushantshah222 <sushant.shah222@gmail.com>
2024-05-31 08:57:01 +0000

    created Publisher and Subscriber node

---
+++ first_workspace/build/first_package/build/lib/first_package/subscriber.py
@@ -0,0 +1,29 @@
+# create a subscriber node that subscribes to the topic "chatter" and prints the message to the console
+
+import rclpy
+from rclpy.node import Node
+
+
+from std_msgs.msg import String
+
+class Subscriber(Node):
+         
+        def __init__(self):
+            super().__init__('subscriber')
+            self.subscription = self.create_subscription(String, 'chatter', self.listener_callback, 10)
+            self.subscription  # prevent unused variable warning
+    
+        def listener_callback(self, msg):
+            self.get_logger().info('I heard: "%s"' % msg.data)
+
+def main(args=None):
+    rclpy.init(args=args)
+    subscriber = Subscriber()
+    rclpy.spin(subscriber)
+    subscriber.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
+
+# End of subscriber.py
